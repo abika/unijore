362f52f06920e66956be4f5977881dea764f2a90
Alexander Bikadorov <alex.bikadorov@kdemail.net>
2024-11-01 14:01:50 +0100

    Make this a Spring Boot project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
+
+    id("org.springframework.boot") version "3.3.5"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "org.abika"
@@ -11,26 +11,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
-}
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.5"))
 
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "ujodsri"
-            packageVersion = "1.0.0"
-        }
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/kotlin/ujodsri/Application.kt
@@ -0,0 +1,27 @@
+package ujodsri
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import kotlin.jvm.java
+
+/**
+ * @author Alexander Bikadorov {@literal <goto@openmailbox.org>}
+ */
+@SpringBootApplication
+class Application {
+
+    private val log = LoggerFactory.getLogger(Application::class.java)
+
+    @Bean
+    fun init() = CommandLineRunner {
+       log.info("Hello World!")
+    }
+
+}
+
+fun main() {
+    runApplication<Application>()
+}
